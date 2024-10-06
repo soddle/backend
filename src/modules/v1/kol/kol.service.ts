99632274f84fe21be6c951085b141392795e06a1
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2024-10-06 13:52:04 +0100

    fixed issue with kols

--- src/modules/v1/kol/kol.service.ts
+++ src/modules/v1/kol/kol.service.ts
@@ -27,9 +27,16 @@ export class KolService {
   getFollowerRangeLabel(followersCount: number): string {
     if (followersCount >= 5000000) return 'over 5M';
     if (followersCount >= 3000000 && followersCount <= 5000000) return '3-5M';
-    if (followersCount >= 1000000 && followersCount < 3000000) return '1-3M';
-    if (followersCount >= 500000 && followersCount < 1000000) return '500k-1M';
-    return '0-500k';
+    if (followersCount >= 2000000 && followersCount < 3000000) return '2-3M';
+    if (followersCount >= 1000000 && followersCount < 2000000) return '1-2M';
+    if (followersCount > 500000 && followersCount < 1000000) return '500k-1M';
+    if (followersCount >= 300000 && followersCount < 500000) return '300k-500k';
+    if (followersCount >= 200000 && followersCount < 300000) return '200k-300k';
+    if (followersCount >= 100000 && followersCount < 200000) return '100k-200k';
+    if (followersCount >= 50000 && followersCount < 100000) return '50k-100k';
+    if (followersCount >= 0 && followersCount < 50000) return '0-50k';
+    if (followersCount >= 500000) return 'over 500k';
+    return '0-100k';
   }
 
   async findAll(): Promise<any[]> {
